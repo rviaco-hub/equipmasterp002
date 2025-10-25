0fc7e3f87b3269d7250bc1412684676d3266244f
admon fixer <cscbarranquilla@fixer.repair>
2025-10-25 16:06:57 -0500

    -_

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb+srv://equipmaster_users:*************@cluster0.sdafji4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    const conn = mongoose.connect("mongodb+srv://equipmaster_users:*************@cluster0.sdafji4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
       .then(() => console.log("✅ Mongo conectado"))
       .catch(err => console.error("❌ Error al conectar Mongo:", err));
 
