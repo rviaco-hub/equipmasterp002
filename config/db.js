c6fb184acd918aba4b7ecf843e995bbb37f93679
admon fixer <cscbarranquilla@fixer.repair>
2025-10-25 16:14:20 -0500

    -_-_

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://equipmaster_users:*************@cluster0.sdafji4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`conn 200`);
   } catch (error) {
     console.error(`Error 500: ${error}`);
