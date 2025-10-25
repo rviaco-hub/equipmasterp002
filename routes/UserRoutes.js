62734c8d5d5b200043a861a0cbd6dc00c6d0d260
admon fixer <cscbarranquilla@fixer.repair>
2025-10-25 05:50:06 -0500

    251025550

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,6 +3,13 @@ import { registerUser, loginUser } from "../controllers/UserController.js";
 
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Backend funcionando correctamente en Render",
+    timestamp: new Date().toISOString(),
+  });
+});
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
